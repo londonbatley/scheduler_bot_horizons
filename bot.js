65d06a6ffbaa46b393dcff141e7a02332fed99dd
Kamran Kara-Pabani <kamrankara@yahoo.com>
2017-08-02 09:44:31 -0700

    Sahir code

--- bot.js
+++ bot.js
@@ -1,99 +1,47 @@
-var slack_token = process.env.SLACK_API_TOKEN;
 var RtmClient = require('@slack/client').RtmClient;
-var WebClient = require('@slack/client').WebClient;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-var rtm = new RtmClient(process.env.SLACK_BOT_TOKEN);
-var web = new WebClient(process.env.SLACK_BOT_TOKEN);
-let channel;
-// The client will emit an RTM.AUTHENTICATED event on successful connection, with the `rtm.start` payload
-rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
-  for (const c of rtmStartData.channels) {
-      if (c.is_member && c.name ==='general') { channel = c.id }
-  }
-  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
-});
-// rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function () {
-//   rtm.sendMessage("MUA HAHAHAHAHA!", channel);
-// });
-// curl 'https://api.api.ai/api/query?v=20150910&query=remind%20me%20to%20text%20nipun&lang=en&sessionId=b73beb52-87db-4cf2-b81d-daadb9400c50&timezone=2017-08-01T16:11:17-0700' -H 'Authorization:Bearer aa01d2ef2709421da6dd98e167c32b32'
-var { User } = require('./models')
-var axios = require('axios')
-// })
-// this is the interactive message
+var bodyParser = require('body-parser');
+
+// const google = require('googleapis');
+// const googleAuth = require('google-auth-library');
+
+const axios = require('axios');
+
+var bot_token = process.env.SLACK_BOT_TOKEN;
+
+// const auth = new googleAuth();
+var rtm = new RtmClient(bot_token); // use my own (snoop-dogg's) token to connect
+
+
+
+function AIquery(str, sessionId) {
+  return axios.post('https://api.api.ai/v1/query?v=20150910', {
+    query: str,
+    sessionId: sessionId,
+    lang: 'en',
+    timezone: 'America/Chicago'
+}, {
+    headers: {
+      "Authorization": `Bearer ${process.env.API_AI_TOKEN}`
+    }
+  } 
+)
+}
+
+
+// Makes Snoop live
 rtm.on(RTM_EVENTS.MESSAGE, function(message) {
-  console.log('message is:',message);
-//dm = Direct message
-var dm = rtm.dataStore.getDMByUserId(message.user);
-//ignore direct messages
-if(!dm ||dm.id !== message.channel || message.type !='message') {
-  console.log('Message ignored')
-  return;
-}
-// axios.post('https://api.api.ai/api/query?v=20150910', {
-//       query: message.text,
-//       lang: 'en',
-//       sessionId: message.user,
-//       timezone: 'America/Chicago'
-//
-//     }
-//   , {
-//     headers: {
-//       'Authorization': `Bearer ${process.env.API_AI_TOKEN}`
-//     }
-//   })
-User.findOne({slackId: message.user})
-  .then(function({data}) {
-console.log('DATAAAAAAAA:', data)
-    if(data.result.actionIncomplete ) {
-          rtm.sendMessage(data.result.fulfillment.speech, message.channel)
-        }
-    // if(data.result.parameters={}){
-    //
-    //   rtm.sendMessage(data.result.fulfillment.speech, message.channel)
-    // }
-    else{
-          console.log('Action is incomplete: ', data.result)
-          web.chat.postMessage(message.channel,'Creating reminder for ' + data.result.parameters.subject + ' on ' + data.result.parameters.date, {
-            'attachments': [{
-                "text": "sup dude, create reminder or cancel?",
-                "fallback": "You are unable to choose an action",
-                "callback_id": "action",
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [
-                  {
-                  "name": "action",
-                  "text": "Confirm",
-                    "type": "button",
-                   "value": "confirm"
-                    },
-                    {
-                      "name": "action",
-                      "text": "Cancel",
-                      "type": "button",
-                      "value": "cancel"
-                    }
-              ]
-            }]
-          },
-          function(err, res) {
-            if(err){
-              console.log('I am in post error: ', err)
-            }else{
-              console.log('interactive sent')
-            }
-          }
-        )
-        }
+
+  AIquery(message.text, message.user)
+  // pass message object into AIquery function
+  .then(function(response) {
+    rtm.sendMessage(response.data.result.fulfillment.speech, message.channel);
+  }).catch(function(err) {
+    console.log('This is the error: ', err);
   })
-  .catch(function(err) {
-    console.log("this is an error", err);
-  })
+
 })
-rtm.start();
-module.export={
-  rtm: rtm
-  // web: web
-}
-// use ngrok forwarding https://
+
+
+rtm.start()
